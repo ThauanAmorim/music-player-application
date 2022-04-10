bc5f8d92d6dbeabe374da4dae3d36c41d6ddc520
Thauan Amorim <thauanssussu@gmail.com>
2022-04-09 23:32:31 -0300

    teste de react

---
+++ music-player/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SideBar } from './components/Sidebar';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <SideBar text='Teste de Fogo' />
+    </div>
+  );
+};
+
+export default App;
---
+++ music-player/src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {
+    text: string;
+}
+
+export const SideBar: React.FC<Props> = ({text}) => {
+    return (
+        <div>
+            
+        </div>
+    ); 
+}
